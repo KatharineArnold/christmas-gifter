2d5affdb718d6de099d13cc421e1d62e4c4920dd
Matt Arnold <marnold@gospotcheck.com>
2019-11-16 11:57:47 -0700

    Remove selection logic

--- client/src/components/RandomSelector/RandomSelector.js
+++ client/src/components/RandomSelector/RandomSelector.js
@@ -1,117 +1,32 @@
-
-
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import "./RandomSelector.css";
+import './RandomSelector.css';
 import ShowSelected from '../ShowSelected/ShowSelected';
 import axios from 'axios';
 
-
 class RandomSelector extends Component {
+  state = {
+    revealed: false,
+  };
 
-    state = {
-        match: "",
+  handleSubmit = () => {
+    this.setState({ revealed: true });
+  };
+
+  render() {
+    let content = <ShowSelected match={this.props.gifter.match}></ShowSelected>;
+
+    if (!this.state.revealed) {
+      content = (
+        <div className="reveal">
+          <button className="revealBtn btn btn-lg" onClick={this.handleSubmit}>
+            {this.props.gifter.name} click to reveal your random match
+          </button>
+        </div>
+      );
     }
-
-
-    // getRandom = () => {
-
-    // }
-
-
-    handleSubmit = () => {
-        axios.get('/api/gifters').then((res) => {
-            // let family = ["Gina", "Keith", "Angela", "Matt", "Katharine"];
-            let family = res.data;
-
-            let removeAll = []
-            for (let i = 0; i < family.length; i++) {
-                if (family[i].match) {
-                    let remove = family[i].match
-                    removeAll.push(remove)
-                }
-            }
-
-            for (let i = 0; i < this.props.gifter.doNotMatchWith.length; i++) {
-                let doNotMatch = this.props.gifter.doNotMatchWith[i];
-                removeAll.push(doNotMatch)
-            }
-
-            for (let i = 0; i < removeAll.length; i++) {
-                let matchedName = removeAll[i];
-                family = family.filter(function (value, index, arr) {
-
-                    return value.name !== matchedName;
-
-                });
-            }
-
-
-
-            console.log(family)
-            let yourMatch;
-
-            // if (this.props.gifter.name === "Gina") {
-            //     yourMatch = "Alex"
-            // }
-            // if (this.props.gifter.name === "Matt") {
-            //     // family = ["Gina", "Keith", "Angela"]
-            // }
-            // if (this.props.gifter.name === "Katharine") {
-            //     // family = ["Gina", "Keith", "Angela"]
-            // }
-            // if (this.props.gifter.name === "Keith") {
-            //     // family = ["Angela", "Matt", "Katharine"]
-            // }
-            // if (this.props.gifter.name === "Alex") {
-            //     // family = ["Gina", "Keith", "Matt", "Katharine"]
-            // }
-
-            yourMatch = family[Math.floor(Math.random() * family.length)].name;
-
-
-            // let newFamily = family.filter(e => e !== yourMatch);
-
-
-
-
-
-            this.setState({ match: yourMatch })
-
-            // what is this?? need to change function on page ??
-            this.props.updateGifter({
-                match: yourMatch,
-                matched: true
-            });
-        });
-
-    }
-
-    render() {
-
-
-        let content = (
-            <ShowSelected match={this.props.gifter.match}></ShowSelected>
-        )
-
-        if (!this.props.gifter.matched) {
-            content = (
-                <div className="reveal">
-                    <button className="revealBtn btn btn-lg" onClick={this.handleSubmit}>{this.props.gifter.name} click to reveal your random match</button>
-                </div>
-            )
-        }
-        return (
-            content
-        )
-    }
-
+    return content;
+  }
 }
 
-
 export default RandomSelector;
-
-
-
-
-
